ca4a68ad25738b4724d54d75321ce4140f7aee5a
infernosalex <alexscanteie0808@gmail.com>
2026-03-26 11:17:24 +0200

    Initialize password manager project

---
+++ src/main/java/ro/unibuc/pao/Main.java
@@ -0,0 +1,9 @@
+package ro.unibuc.pao;
+
+import ro.unibuc.pao.ui.ConsoleApp;
+
+public class Main {
+    public static void main(String[] args) {
+        new ConsoleApp().run();
+    }
+}
---
+++ src/main/java/ro/unibuc/pao/ui/ConsoleApp.java
@@ -0,0 +1,8 @@
+package ro.unibuc.pao.ui;
+
+public class ConsoleApp {
+    public void run() {
+        System.out.println("Password Manager");
+        System.out.println("Aplicatia va fi initializata in pasii urmatori.");
+    }
+}
